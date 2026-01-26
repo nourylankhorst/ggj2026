ca04b23ab34faf1252a3c5c1f4a2a0d968f6c977
yeetyplayz <remcoklaassen077@gmail.com>
2026-01-26 13:29:20 +0100

    player

--- GGJ2026/Assets/Scripts/Player/Player.cs
+++ GGJ2026/Assets/Scripts/Player/Player.cs
@@ -1,8 +1,7 @@
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    public string testVar;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
