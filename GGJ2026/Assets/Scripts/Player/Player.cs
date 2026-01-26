2f18373be31363c5bbe8d7680c7a7f8b73ceb6ab
yeetyplayz <remcoklaassen077@gmail.com>
2026-01-26 14:04:25 +0100

    sdfx

--- GGJ2026/Assets/Scripts/Player/Player.cs
+++ GGJ2026/Assets/Scripts/Player/Player.cs
@@ -4,16 +4,16 @@ public class Player : MonoBehaviour
 {
     [Header("Player Movement")]
     public float movementSpeed = 4;
-    public float jumpHeight = 7;
-    public bool isGrounded = false;
-    private Rigidbody rb;
+   // public float jumpHeight = 7;
+  //  public bool isGrounded = false;
+   // private Rigidbody rb;
     private Vector3 moveDir;
     private float hor;
     private float vert;
 
     void Start()
     {
-       rb = GetComponent<Rigidbody>(); 
+    //   rb = GetComponent<Rigidbody>(); 
     }
     void Update()
     {
@@ -22,17 +22,17 @@ public class Player : MonoBehaviour
         moveDir.x = hor;
         moveDir.y = vert;
         transform.Translate(moveDir * Time.deltaTime * movementSpeed);
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
-            isGrounded = false;
-        }
+        //if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+       // {
+        //    rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        //    isGrounded = false;
+      //  }
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Terrain"))
-        {
-            isGrounded = true;
-        }
-    }
+   // private void OnCollisionEnter(Collision collision)
+   // {
+    //    if (collision.gameObject.CompareTag("Terrain"))
+     //   {
+     //       isGrounded = true;
+     //   }
+    //}
 }
