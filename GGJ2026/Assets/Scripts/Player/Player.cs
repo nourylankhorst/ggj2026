96fab10b600011f3aa8cec6810eae8a91f6f9a9d
yeetyplayz <remcoklaassen077@gmail.com>
2026-01-26 14:54:10 +0100

    parkour + sprint

--- GGJ2026/Assets/Scripts/Player/Player.cs
+++ GGJ2026/Assets/Scripts/Player/Player.cs
@@ -5,6 +5,7 @@ public class Player : MonoBehaviour
     [Header("Player Movement")]
     public float movementSpeed = 4;
     public float jumpHeight = 7;
+    public float jumps = 0;
     public bool isGrounded = false;
     private Rigidbody rb;
     private Vector3 moveDir;
@@ -26,6 +27,14 @@ public class Player : MonoBehaviour
         {
             rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
             isGrounded = false;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            movementSpeed = 10;
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                movementSpeed = 5;
+            }
         }
     }
     private void OnCollisionEnter(Collision collision)
