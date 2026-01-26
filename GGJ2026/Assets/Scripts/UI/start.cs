a3aa453876a865cb26783cfcb144576f925c0831
yeetyplayz <remcoklaassen077@gmail.com>
2026-01-26 23:02:59 +0100

    added UI stuff
    start menu, (start of a) pause menu

--- GGJ2026/Assets/Scripts/UI/start.cs
+++ GGJ2026/Assets/Scripts/UI/start.cs
@@ -3,11 +3,33 @@ using UnityEngine;
 public class start : MonoBehaviour
 {
     // every script requires one public (class name) (<==);
+    public bool paused = false;
     public Player player;
     public PlayerCam playerCam;
+    public GameObject startUI;
+    public GameObject pauseUI;
 
-    public void StartGame()
+    public void Start()
     {
         player.enabled = false;
+        playerCam.enabled = false;
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+            Time.timeScale = paused ? 0f : 1f;
+            player.enabled = false;
+            playerCam.enabled = false;
+            pauseUI.SetActive(true);
+        }
+    }
+    public void StartGame()
+    {
+        player.enabled = true;
+        playerCam.enabled = true;
+        pauseUI.SetActive(false);
+        startUI.SetActive(false);
     }
 }
